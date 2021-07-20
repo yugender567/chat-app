bd559034916a9d077412d6134f4b60c478b4601b
yugender567 <yugenderyogi2711@gmail.com>
2021-07-20 17:08:58 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyBWKJRZArEJ1C-yp6Gnrditc4VDwSIgPtI',
+  authDomain: 'chat-web-app-f8242.firebaseapp.com',
+  projectId: 'chat-web-app-f8242',
+  storageBucket: 'chat-web-app-f8242.appspot.com',
+  messagingSenderId: '352533476942',
+  appId: '1:352533476942:web:ca122aab0cb2ca80ed351f',
+  measurementId: 'G-LZL74K609R',
+};
+
+const app = firebase.initializeApp(config);
