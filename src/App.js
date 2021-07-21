1163183cddcbc02da2396e9f42f37b93f44da393
yugender567 <yugenderyogi2711@gmail.com>
2021-07-22 00:39:47 +0530

    created private and public routes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import React from 'react';
 
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
+import { Switch } from 'react-router';
+import Signin from './pages/Signin';
+import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home';
+import PublicRoute from './components/PublicRoute';
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <Switch>
+      <PublicRoute path="/signin">
+        <Signin />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
+  );
 }
 
 export default App;
---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router';
+
+const PrivateRoute = ({ children, ...routeprops }) => {
+  const profile = false;
+  if (!profile) {
+    return <Redirect to="/signin" />;
+  }
+  return <Route {...routeprops}>{children}</Route>;
+};
+
+export default PrivateRoute;
