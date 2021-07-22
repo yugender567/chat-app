7d023ae340766e3172fc11ad679147d17fdbc142
yugender567 <yugenderyogi2711@gmail.com>
2021-07-22 17:40:27 +0530

    added sign in page and sign in with facebook/Google

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,8 +1,12 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
   apiKey: 'AIzaSyBWKJRZArEJ1C-yp6Gnrditc4VDwSIgPtI',
   authDomain: 'chat-web-app-f8242.firebaseapp.com',
+  databaseURL:
+    'https://chat-web-app-f8242-default-rtdb.asia-southeast1.firebasedatabase.app/',
   projectId: 'chat-web-app-f8242',
   storageBucket: 'chat-web-app-f8242.appspot.com',
   messagingSenderId: '352533476942',
@@ -11,3 +15,5 @@ const config = {
 };
 
 const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,7 +1,58 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import { Alert, Button, Col, Container, Grid, Icon, Panel, Row } from 'rsuite';
+import { auth, database } from '../misc/firebase';
 
 const Signin = () => {
-  return <div>sign in</div>;
+  const signInWithProvider = async provider => {
+    try {
+      // const result = await auth.signInWithPopup(provider);
+      // console.log(result);
+
+      const { additionalUserInfo, user } = await auth.signInWithPopup(provider);
+      if (additionalUserInfo.isNewUser) {
+        await database.ref(`/profiles/${user.uid}`).set({
+          name: user.displayName,
+          createdAt: firebase.database.ServerValue.TIMESTAMP,
+        });
+      }
+
+      Alert.success('Signed in', 4000);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
+  };
+  const onFacebookSignIn = () => {
+    signInWithProvider(new firebase.auth.FacebookAuthProvider());
+  };
+  const onGoogleSignIn = () => {
+    signInWithProvider(new firebase.auth.GoogleAuthProvider());
+  };
+
+  return (
+    <Container>
+      <Grid className="mt-page">
+        <Row>
+          <Col xs={24} md={12} mdOffset={6}>
+            <Panel>
+              <div className="text-center">
+                <h2>Welcome to Chat</h2>
+                <p>progressive chat platform for neophytes</p>
+              </div>
+              <div className="mt-3">
+                <Button block color="green" onClick={onGoogleSignIn}>
+                  <Icon icon="google" /> Continue with Google
+                </Button>
+                <Button block color="blue" onClick={onFacebookSignIn}>
+                  <Icon icon="facebook" /> Continue with Facebook
+                </Button>
+              </div>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
+  );
 };
 
 export default Signin;
