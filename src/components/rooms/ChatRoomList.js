8cc5769cae379ce22281002b4612d30160137e1e
yugender567 <yugenderyogi2711@gmail.com>
2021-07-26 22:42:42 +0530

    created rooms context

--- src/components/rooms/ChatRoomList.js
+++ src/components/rooms/ChatRoomList.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Nav } from 'rsuite';
 import RoomItem from './Roomitem';
 
-const ChatRoomList = () => {
+const ChatRoomList = ({ aboveElHeight }) => {
   return (
     <Nav
       appearance="subtle"
       vertical
       reversed
       className="overflow-y-scroll custom-scroll"
+      style={{
+        height: `calc(100%-${aboveElHeight}px)`,
+      }}
     >
       <Nav.Item>
         <RoomItem />
---
+++ src/context/rooms.context.js
@@ -0,0 +1,26 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { database } from '../misc/firebase';
+import { transformToArrWithId } from '../misc/helpers';
+
+const RoomsContext = createContext();
+
+export const RoomsProvider = ({ children }) => {
+  const [rooms, setRooms] = useState(null);
+  useEffect(() => {
+    const roomListRef = database.ref('rooms');
+
+    roomListRef.on('value', snap => {
+      const data = transformToArrWithId(snap.val());
+      setRooms(data);
+      // console.log('data',data)  here its give array
+      // console.log('snap.val()', snap.val());
+    });
+    return () => {
+      roomListRef.off();
+    };
+  }, []);
+
+  return (
+    <RoomsContext.Provider value={rooms}>{children}</RoomsContext.Provider>
+  );
+};
